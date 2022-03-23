65ea60dd18da966fcd42516ce3b0d47c1792c526
javiercmch06 <javiercmch06@gmail.com>
2022-03-23 11:07:09 +0100

    Initial commit

---
+++ app/src/main/java/com/uc3m/aptel/madbreak/MainActivity.java
@@ -0,0 +1,14 @@
+package com.uc3m.aptel.madbreak;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
